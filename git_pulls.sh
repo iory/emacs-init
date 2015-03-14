3bb092593172baab1f0f53bdf586b8a7cdbc2a34
iori <ab.ioryz@gmail.com>
2015-03-15 06:33:13 +0900

    add mkdir to use trr

--- git_pulls.sh
+++ git_pulls.sh
@@ -1,4 +1,6 @@
-cd site-lisp
+mkdir ~/.emacs.d/site-lisp
+cd ~/.emacs.d/site-lisp
 git clone git@github.com:emacs-helm/helm-ls-git.git
 git clone git@github.com:kawabata/emacs-trr.git
 git clone --recursive git@github.com:capitaomorte/yasnippet.git
+mkdir -p ~/.emacs.d/games/trrscores
