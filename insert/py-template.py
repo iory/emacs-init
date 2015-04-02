89c09f059a2e4374f9962af13a89e5357067799c
iori <ab.ioryz@gmai.com>
2015-04-02 12:23:44 +0900

    delete unnecessary insert-code

--- insert/py-template.py
+++ insert/py-template.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import math
-import string
-import itertools
-import fractions
-import heapq
-import collections
-import re
-import array
-import bisect
-import os
-import sys
-import scipy as sp
-import numpy as np
-import matplotlib.pyplot as plt
-import sklearn
-
 def main():
     print("hello python!")
 
