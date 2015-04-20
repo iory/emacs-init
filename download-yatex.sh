9428454a00d445afe3225d2ebc37996852538b0f
iory <ab.ioryz@gmail.com>
2015-04-20 11:05:01 +0900

    add sudo apt-get

--- download-yatex.sh
+++ download-yatex.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# tex install
+sudo apt-get -y install texlive
+sudo apt-get -y install texlive-lang-cjk
+sudo apt-get -y install xdvik-ja
+sudo apt-get -y install dvipsk-ja
+sudo apt-get -y install gv
+sudo apt-get -y install texlive-fonts-recommended texlive-fonts-extra
+
+# yatex install to site-lisp
 wget http://www.yatex.org/yatex1.78.4.tar.gz
 tar xvzf yatex1.78.4.tar.gz
 mv yatex1.78.4 site-lisp/yatex
