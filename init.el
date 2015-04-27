866b634b9228095b4ed54115b2243cb7075939fc
iory <ab.ioryz@gmail.com>
2015-04-27 10:47:22 +0900

    modified tab->space and visalize hard tab

--- init.el
+++ init.el
@@ -1,3 +1,96 @@
+(require 'cl)
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+(defvar installing-package-list
+  '(
+    ;; package list to use
+    whitespace
+    ))
+
+;; automatically install
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
+
+(add-to-list 'load-path "~/.emacs.d/elpa/")
 (add-to-list 'load-path "/opt/ros/indigo/share/emacs/site-lisp")
 ;; or whatever your install space is + "/share/emacs/site-lisp"
 (require 'rosemacs-config)
+
+;; inhibit startup screen
+(setq inhibit-startup-screen t)
+
+;; delete initial scratch message
+(setq initial-scratch-message "")
+
+
+;; tab->space
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; whitespace settings
+(require 'whitespace)
+(setq whitespace-style '(face           ; faceで可視化
+                         trailing       ; 行末
+                         tabs           ; タブ
+                         spaces         ; スペース
+                         empty          ; 先頭/末尾の空行
+                         space-mark     ; 表示のマッピング
+                         tab-mark
+                         ))
+
+(setq whitespace-display-mappings
+      '((space-mark ?\u3000 [?\u25a1])
+        ;; WARNING: the mapping below has a problem.
+        ;; When a TAB occupies exactly one column, it will display the
+        ;; character ?\xBB at that column followed by a TAB which goes to
+        ;; the next TAB column.
+        ;; If this is a problem for you, please, comment the line below.
+        (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+
+;; スペースは全角のみを可視化
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+
+;; 保存前に自動でクリーンアップ
+(setq whitespace-action '(auto-cleanup))
+
+(global-whitespace-mode 1)
+
+(defvar my/bg-color "#232323")
+(set-face-attribute 'whitespace-trailing nil
+                    :background my/bg-color
+                    :foreground "DeepPink"
+                    :underline t)
+(set-face-attribute 'whitespace-tab nil
+                    :background my/bg-color
+                    :foreground "LightSkyBlue"
+                    :underline t)
+(set-face-attribute 'whitespace-space nil
+                    :background my/bg-color
+                    :foreground "GreenYellow"
+                    :weight 'bold)
+(set-face-attribute 'whitespace-empty nil
+                    :background my/bg-color)
+
+
+(setq
+   ;; クリップボードでコピー＆ペーストできるようにする
+   x-select-enable-clipboard t
+   ;; PRIMARY selectionを使う(Windowsでは対象外)
+   x-select-enable-primary t
+   ;; クリップボードでコピー・カットした文字列を
+   ;; キルリングにも保存させる
+   save-interprogram-paste-before-kill t
+   ;; エラー時などはベル音ではなくて画面を1回点滅させる
+   visible-bell t
+   ;; バックアップファイルはカレントディレクトリではなく
+   ;; ~/.emacs.d/backups 以下に保存する
+   backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                            "backups"))))
