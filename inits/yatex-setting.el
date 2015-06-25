c7e150b2fcce5624e9257ec2c5ca9302538fbbf1
iory <ab.ioryz@gmail.com>
2015-06-26 07:19:24 +0900

    add yatex code setting

--- inits/yatex-setting.el
+++ inits/yatex-setting.el
@@ -17,4 +17,7 @@
     (setq dvi2-command "open -a Preview")))
 (add-hook 'yatex-mode-hook '(lambda () (setq auto-fill-function nil)))
 
+;; set YaTeX coding system
+(setq YaTeX-kanji-code 4)
+
 (provide 'yatex-setting)
