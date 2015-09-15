a0b044685d9f0b9238a4e977ab552799ad128d90
iory <ab.ioryz@gmail.com>
2015-09-16 00:42:42 +0900

    Add template for script file

--- inits/insert-setting.el
+++ inits/insert-setting.el
@@ -9,6 +9,7 @@
 (define-auto-insert "\\.cpp$" "c-plusplus-template.cpp")
 (define-auto-insert "\\.c$" "c-template.c")
 (define-auto-insert "\\.py$" "py-template.py")
+(define-auto-insert "\\.sh$" "sh-template.sh")
 (define-auto-insert "\\.launch$" "launch-template.launch")
 
 (provide 'insert-setting)
